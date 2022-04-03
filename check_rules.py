a49634ef06ffdd768280580da03e23042e61a705
Palagi, Brad <bpalagi2@illinois.edu>
2022-04-03 21:45:00 +0200

    event number

--- check_rules.py
+++ check_rules.py
@@ -1,91 +1,89 @@
-# import sys
-# from github import Github
-
-
-# def main():
-
-#     print (sys.argv[2])
-
-#     github = Github(sys.argv[1])
-#     repo = github.get_repo('KTH/devops-course', lazy=False)
-
-#     pr = repo.get_pull(1650)
-
-#     print(pr.body)
-
-#     # num_students = 0
-
-#     # words = pr.body.split()
-
-#     # for word in words:
-#     #     if '@kth.se' in word:
-#     #         num_students+=1
-
-#     # print ('Num students: ', num_students)
-
-# if __name__ == "__main__":
-#     main()
-'''
-We will create a script to enforce the submission rules of the course.
-
-These include:
-1. Ensuring that students are not partners with the same person for more than two projects
-2. Ensuring that students are not working alone for more than two projects
-3. Ensuring that a student is not choosing the same topic (ex. Testing & CI, Containers & Serverless, etc.) for two different tasks
-4. Making sure that a student has not done more a task (ex. presentation, essay, etc.) more than once
-'''
-
 import sys
-import re
-
-def find_students(students: list, text: str):
-    words = text.split()
-    for word in words:
-        if '@kth.se' in word:
-            name = ''
-            for char in word.split('@')[0]:
-                if char.isalpha():
-                    name += char
-            students.append(name)
-    return students
+from github import Github
 
 
-def check_partners():
+def main():
 
-    return True
-
-# print(sys.argv[1])
-
-example_body = '''
-# Assignment Proposal
-
-## Title
-
-DevOps in healthcare technology
-
-## Names and KTH ID
-
-<<<<<<< HEAD
     print (sys.argv[2].number)
-=======
-- Lucas Eren (leren@kth.se)
-- Sanherib Elia (sanherib@kth.se)
->>>>>>> 8aebeeb10dccb2e898605f0166ec2563dc1fd53d
 
-## Deadline
+    github = Github(sys.argv[1])
+    repo = github.get_repo('KTH/devops-course', lazy=False)
 
-Task 1 (April 5)
+    pr = repo.get_pull(1650)
 
-## Category
+    print(pr.body)
 
-Week 3: Continuous Deployment / Delivery and Feature flags (April 5)
+    # num_students = 0
 
-## Description
+    # words = pr.body.split()
 
-The manufacture of medical devices is a strictly regulated domain in the European Union. Medical Devices traditionally follow the Waterfall methodology of development, where design is predefined, testing is often manual and updates can take months to roll out due to a bloated architecture. This is where DevOps solves these problems by making the architecture leaner and allowing for teams to make changes faster and more efficiently. It allows teams to be able to meet changing market needs and factor in feedback. 
+    # for word in words:
+    #     if '@kth.se' in word:
+    #         num_students+=1
 
-We want to give a presentation on how the healthcare industry can (and has) implemented a DevOps methodology to improve software quality. We will discuss DevOps generally but focus on CI/CD pipelines for healthcare software. 
-'''
+    # print ('Num students: ', num_students)
 
-students = []
-print(find_students(students, example_body))
+if __name__ == "__main__":
+    main()
+
+
+
+# '''
+# We will create a script to enforce the submission rules of the course.
+
+# These include:
+# 1. Ensuring that students are not partners with the same person for more than two projects
+# 2. Ensuring that students are not working alone for more than two projects
+# 3. Ensuring that a student is not choosing the same topic (ex. Testing & CI, Containers & Serverless, etc.) for two different tasks
+# 4. Making sure that a student has not done more a task (ex. presentation, essay, etc.) more than once
+# '''
+
+# import sys
+# import re
+
+# def find_students(students: list, text: str):
+#     words = text.split()
+#     for word in words:
+#         if '@kth.se' in word:
+#             name = ''
+#             for char in word.split('@')[0]:
+#                 if char.isalpha():
+#                     name += char
+#             students.append(name)
+#     return students
+
+
+# def check_partners():
+
+#     return True
+
+# # print(sys.argv[1])
+
+# example_body = '''
+# # Assignment Proposal
+
+# ## Title
+
+# DevOps in healthcare technology
+
+# ## Names and KTH ID
+
+#     print (sys.argv[2].number)
+
+# ## Deadline
+
+# Task 1 (April 5)
+
+# ## Category
+
+# Week 3: Continuous Deployment / Delivery and Feature flags (April 5)
+
+# ## Description
+
+# The manufacture of medical devices is a strictly regulated domain in the European Union. Medical Devices traditionally follow the Waterfall methodology of development, where design is predefined, testing is often manual and updates can take months to roll out due to a bloated architecture. This is where DevOps solves these problems by making the architecture leaner and allowing for teams to make changes faster and more efficiently. It allows teams to be able to meet changing market needs and factor in feedback. 
+
+# We want to give a presentation on how the healthcare industry can (and has) implemented a DevOps methodology to improve software quality. We will discuss DevOps generally but focus on CI/CD pipelines for healthcare software. 
+# '''
+
+# students = []
+# print(find_students(students, example_body))
