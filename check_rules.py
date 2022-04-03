822cfdb9b200b751e202293bca7731c6d71aca8f
Ben Civjan <bcivjan2@illinois.edu>
2022-04-03 19:56:21 +0200

    Update script

---
+++ check_rules.py
@@ -0,0 +1,17 @@
+'''
+We will create a script to enforce the submission rules of the course.
+
+These include:
+1. Ensuring that students are not partners with the same person for more than two projects
+2. Ensuring that students are not working alone for more than two projects
+3. Ensuring that a student is not choosing the same topic (ex. Testing & CI, Containers & Serverless, etc.) for two different tasks
+4. Making sure that a student has not done more a task (ex. presentation, essay, etc.) more than once
+'''
+
+import sys
+
+def check_partners():
+
+    return True
+
+print(sys.argv[0])
