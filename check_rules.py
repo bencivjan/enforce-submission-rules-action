9328acb6385c46394fa8a9ae6a238140773a8d4e
Palagi, Brad <bpalagi2@illinois.edu>
2022-04-03 21:49:44 +0200

    event number

--- check_rules.py
+++ check_rules.py
@@ -4,24 +4,24 @@ from github import Github
 
 def main():
 
-    print (sys.argv[2].number)
-
     github = Github(sys.argv[1])
     repo = github.get_repo('KTH/devops-course', lazy=False)
 
+    # Change once on PR   
+    # pr = repo.get_pull(sys.argv[2])
     pr = repo.get_pull(1650)
 
-    print(pr.body)
+    # print(pr.body)
 
-    # num_students = 0
+    num_students = 0
 
-    # words = pr.body.split()
+    words = pr.body.split()
 
-    # for word in words:
-    #     if '@kth.se' in word:
-    #         num_students+=1
+    for word in words:
+        if '@kth.se' in word:
+            num_students+=1
 
-    # print ('Num students: ', num_students)
+    print ('Num students: ', num_students)
 
 if __name__ == "__main__":
     main()
