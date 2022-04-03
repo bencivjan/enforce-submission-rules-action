39fc17d3ed643a193e7c198a7b87692c2c1f16b2
Ben Civjan <bcivjan2@illinois.edu>
2022-04-03 21:28:31 +0200

    Created name parse function

--- check_rules.py
+++ check_rules.py
@@ -9,9 +9,53 @@ These include:
 '''
 
 import sys
+import re
+
+students = []
+
+def find_students(text: str):
+    words = text.split()
+    for word in words:
+        if '@kth.se' in word:
+            name = ''
+            for char in word.split('@')[0]:
+                if char.isalpha():
+                    name += char
+            students.append(name)
+
 
 def check_partners():
 
     return True
 
-print(sys.argv[0])
+# print(sys.argv[1])
+
+example_body = '''
+# Assignment Proposal
+
+## Title
+
+DevOps in healthcare technology
+
+## Names and KTH ID
+
+- Lucas Eren (leren@kth.se)
+- Sanherib Elia (sanherib@kth.se)
+
+## Deadline
+
+Task 1 (April 5)
+
+## Category
+
+Week 3: Continuous Deployment / Delivery and Feature flags (April 5)
+
+## Description
+
+The manufacture of medical devices is a strictly regulated domain in the European Union. Medical Devices traditionally follow the Waterfall methodology of development, where design is predefined, testing is often manual and updates can take months to roll out due to a bloated architecture. This is where DevOps solves these problems by making the architecture leaner and allowing for teams to make changes faster and more efficiently. It allows teams to be able to meet changing market needs and factor in feedback. 
+
+We want to give a presentation on how the healthcare industry can (and has) implemented a DevOps methodology to improve software quality. We will discuss DevOps generally but focus on CI/CD pipelines for healthcare software. 
+'''
+
+find_students(example_body)
+print(students)
