c8b7c31d7fd80a4ec351f47f049f298a990e882d
Palagi, Brad <bpalagi2@illinois.edu>
2022-04-03 21:33:25 +0200

    github.event sysarg

--- check_rules.py
+++ check_rules.py
@@ -1,28 +1,25 @@
 import sys
 from github import Github
 
-github = Github(sys.argv[1])
-repo = github.get_repo('KTH/devops-course', lazy=False)
 
-pr = repo.get_pull(1650)
+def main():
 
-# commits = pr.get_commits()
+    print (sys.argv[2])
 
-num_students = 0
+    github = Github(sys.argv[1])
+    repo = github.get_repo('KTH/devops-course', lazy=False)
 
-words = pr.body.split()
+    pr = repo.get_pull(1650)
 
-for word in words:
-    if '@kth.se' in word:
-        num_students+=1
+    num_students = 0
 
-print ('Num students: ', num_students)
+    words = pr.body.split()
 
+    for word in words:
+        if '@kth.se' in word:
+            num_students+=1
 
-# for commit in commits:
-#     files = commit.files
-#     for file in files:
-#         filename = file.filename
-#         contents = repo.get_contents(filename, ref=commit.sha).decoded_content
+    print ('Num students: ', num_students)
 
-            
+if __name__ == "__main__":
+    main()
