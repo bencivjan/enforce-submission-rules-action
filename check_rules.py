8469f0872e278b0dd291b0311439d88c2f1b6030
Palagi, Brad <bpalagi2@illinois.edu>
2022-04-04 16:26:36 +0200

    soloCheck() and partnerCheck()

--- check_rules.py
+++ check_rules.py
@@ -1,5 +1,124 @@
+from ctypes import sizeof
 import sys
 from github import Github
+
+
+# No student works alone more than twice
+def soloCheck(name, repo):
+    num_times_alone = 0
+
+    # contents should contain all folders contained in /contributions
+    contents = repo.get_contents("contributions/course-automation")
+    contents += repo.get_contents("contributions/essay")
+    contents += repo.get_contents("contributions/executable-tutorial")
+    contents += repo.get_contents("contributions/feedback")
+    contents += repo.get_contents("contributions/open-source")
+
+    # check folders containing this students name
+    for word in contents:
+        word = str(word)[len("ContentFile(path=\"contributions/"):len(str(word))].split("/")
+        if name in word[1]:
+            # determine if this was also a solo project
+            names = word[1].split("-")
+            if len(names) == 1:
+                num_times_alone+=1
+
+    contents = repo.get_contents("contributions/presentation/week2-testing-and-CI")
+    contents += repo.get_contents("contributions/presentation/week3-CD-and-feature-flag")
+    contents += repo.get_contents("contributions/presentation/week4-containers-and-serverless")
+    contents += repo.get_contents("contributions/presentation/week5-Infrastructure-as-Code")
+    contents += repo.get_contents("contributions/presentation/week6-software-bots")
+    contents += repo.get_contents("contributions/presentation/week7-dependency-DevSecOps")
+    contents += repo.get_contents("contributions/presentation/week8-culture-and-legal")
+    contents += repo.get_contents("contributions/presentation/week9-other")
+
+    for word in contents:
+        word = str(word)[len("ContentFile(path=\"contributions/presentation/"):len(str(word))].split("/")
+        if name in word[1]:
+            # determine if this was also a solo project
+            names = word[1].split("-")
+            if len(names) == 1:
+                num_times_alone+=1
+
+    contents = repo.get_contents("contributions/demo/week2-testing-and-CI")
+    contents += repo.get_contents("contributions/demo/week3-CD-and-feature-flag")
+    contents += repo.get_contents("contributions/demo/week4-containers-and-serverless")
+    contents += repo.get_contents("contributions/demo/week5-Infrastructure-as-Code")
+    contents += repo.get_contents("contributions/demo/week6-software-bots")
+    contents += repo.get_contents("contributions/demo/week7-dependency-DevSecOps")
+    contents += repo.get_contents("contributions/demo/week8-culture-and-legal")
+    contents += repo.get_contents("contributions/demo/week9-other")
+
+    for word in contents:
+        word = str(word)[len("ContentFile(path=\"contributions/demo/"):len(str(word))].split("/")
+        if name in word[1]:
+            # determine if this was also a solo project
+            names = word[1].split("-")
+            if len(names) == 1:
+                num_times_alone+=1
+
+    print("Number of times ", name, "worked alone: ", num_times_alone)
+
+
+    if num_times_alone >=2:
+        print ("Student may not work alone again")
+    else:
+        print ("Student okay to work alone")
+
+
+# No two students work together more than twice
+def partnerCheck(students, repo):
+    num_times_together = 0
+
+    # contents should contain all folders contained in /contributions
+    contents = repo.get_contents("contributions/course-automation")
+    contents += repo.get_contents("contributions/essay")
+    contents += repo.get_contents("contributions/executable-tutorial")
+    contents += repo.get_contents("contributions/feedback")
+    contents += repo.get_contents("contributions/open-source")
+
+    # check folders containing this students name
+    for word in contents:
+        word = str(word)[len("ContentFile(path=\"contributions/"):len(str(word))].split("/")
+        if students[0] in word[1] and students[1] in word[1]:
+            num_times_together+=1
+
+    contents = repo.get_contents("contributions/presentation/week2-testing-and-CI")
+    contents += repo.get_contents("contributions/presentation/week3-CD-and-feature-flag")
+    contents += repo.get_contents("contributions/presentation/week4-containers-and-serverless")
+    contents += repo.get_contents("contributions/presentation/week5-Infrastructure-as-Code")
+    contents += repo.get_contents("contributions/presentation/week6-software-bots")
+    contents += repo.get_contents("contributions/presentation/week7-dependency-DevSecOps")
+    contents += repo.get_contents("contributions/presentation/week8-culture-and-legal")
+    contents += repo.get_contents("contributions/presentation/week9-other")
+
+    for word in contents:
+        word = str(word)[len("ContentFile(path=\"contributions/presentation/"):len(str(word))].split("/")
+        if students[0] in word[1] and students[1] in word[1]:
+            num_times_together+=1
+
+    contents = repo.get_contents("contributions/demo/week2-testing-and-CI")
+    contents += repo.get_contents("contributions/demo/week3-CD-and-feature-flag")
+    contents += repo.get_contents("contributions/demo/week4-containers-and-serverless")
+    contents += repo.get_contents("contributions/demo/week5-Infrastructure-as-Code")
+    contents += repo.get_contents("contributions/demo/week6-software-bots")
+    contents += repo.get_contents("contributions/demo/week7-dependency-DevSecOps")
+    contents += repo.get_contents("contributions/demo/week8-culture-and-legal")
+    contents += repo.get_contents("contributions/demo/week9-other")
+
+    for word in contents:
+        word = str(word)[len("ContentFile(path=\"contributions/demo/"):len(str(word))].split("/")
+        if students[0] in word[1] and students[1] in word[1]:
+            num_times_together+=1
+
+
+    print("Number of times ", students, "worked together: ", num_times_together)
+
+    if num_times_together >=2:
+        print ("Students may not work together again")
+    else:
+        print ("Student okay to work together")
+
 
 
 def main():
@@ -7,25 +126,29 @@ def main():
     f = open('token.txt','r')
     github = Github(f.readline())
     f.close()
-
     # github = Github(sys.argv[1])
+
     repo = github.get_repo('KTH/devops-course', lazy=False)
 
-    # Change once on PR   
-    # pr = repo.get_pull(sys.argv[2])
-    pr = repo.get_pull(1650)
+    pr_num = 1650
+    # pr_num = 1605
+    # pr_num = sys.argv[2]
 
-    # print(pr.body)
-
-    num_students = 0
-
+    pr = repo.get_pull(pr_num)
     words = pr.body.split()
+    students = []
 
     for word in words:
         if '@kth.se' in word:
-            num_students+=1
+            students.append(word.split('(')[1].split('@')[0])
 
-    print ('Num students: ', num_students)
+    if len(students) == 1:
+        soloCheck(students[0],repo)
+    elif len(students) == 2:
+        partnerCheck(students,repo)
+    else:
+        print('Issue with number of students on the PR')
+
 
 if __name__ == "__main__":
     main()
